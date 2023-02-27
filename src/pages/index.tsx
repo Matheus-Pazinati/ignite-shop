b43d6d01d75cb17c16dba55a4d8bbe197de8057b
Matheus Pazinati <matheuspazinati@gmail.com>
2023-02-27 05:36:46 -0300

    Buscando e exibindo os produtos provindos da API da Stripe, fazendo essa requisicao no lado servidor utilizando o getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
+import { stripe } from '../lib/stripe'
 import { HomeContainer, ProductCard } from '@/styles/pages/home'
 
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
+import { GetServerSideProps } from 'next'
 
 import Image from 'next/image'
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
+import Stripe from 'stripe'
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
 
   const [sliderRef] = useKeenSlider({
     slides: {
@@ -18,50 +27,39 @@ export default function Home() {
   })
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <ProductCard className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
-        <footer>
-          <strong>
-            Camiseta Beyond the Limits
-          </strong>
-          <span>
-            R$ 79,90
-          </span>
-        </footer>
-      </ProductCard>
-      <ProductCard className="keen-slider__slide">
-        <Image src={camiseta2} width={520} height={480} alt="" />
-        <footer>
-          <strong>
-            Camiseta Beyond the Limits
-          </strong>
-          <span>
-            R$ 79,90
-          </span>
-        </footer>
-      </ProductCard>
-      <ProductCard className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
-        <footer>
-          <strong>
-            Camiseta Beyond the Limits
-          </strong>
-          <span>
-            R$ 79,90
-          </span>
-        </footer>
-      </ProductCard>
-      <ProductCard className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
-        <footer>
-          <strong>
-            Camiseta Beyond the Limits
-          </strong>
-          <span>
-            R$ 79,90
-          </span>
-        </footer>
-      </ProductCard>
+      {products.map((product) => (
+        <ProductCard key={product.id} className="keen-slider__slide">
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <footer>
+            <strong>
+              {product.name}
+            </strong>
+            <span>
+              {product.price}
+            </span>
+          </footer>
+        </ProductCard>
+      ))}
     </HomeContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+  const products = await response.data.map((product) => {
+    const productPrice = product.default_price as Stripe.Price
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: productPrice.unit_amount! / 100
+    }
+  })
+  return {
+    props: {
+      products
+    }
+  }
 }
