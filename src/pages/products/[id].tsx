9ae7953fa85fcfc219acc842e4f763e26d255fc5
Matheus Pazinati <matheuspazinati@gmail.com>
2023-03-02 05:17:12 -0300

    Cria estrutura HTML da pagina do produto

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,8 +1,15 @@
-import { useRouter } from "next/router"
-
 export default function Product() {
-  const { query } = useRouter()
   return (
-    <h1>Product: {query.id}</h1>
+    <main>
+      <div>
+        <p>Imagem da camiseta</p>
+      </div>
+      <div>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam in delectus numquam, vel inventore similique recusandae, laborum ipsam sequi doloribus esse provident repellat soluta. Dolores cum quos adipisci ullam modi.</p>
+        <button>Comprar agora</button>
+      </div>
+    </main>
   )
 }
--- src/pages/products/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Products() {
-  return (
-    <h1>All Products</h1>
-  )
-}
