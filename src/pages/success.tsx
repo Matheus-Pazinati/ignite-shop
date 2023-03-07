52decf1c5a239f9e92379b6949a3d57bd4d9feac
Matheus Pazinati <matheuspazinati@gmail.com>
2023-03-07 05:05:49 -0300

    Cria estrutura HTML da pagina de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,12 @@
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <main>
+      <h1>Compra efetuada</h1>
+      <div>
+        <p>Imagem Camiseta</p>
+      </div>
+      <p>Uhuul <strong>Matheus</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa. </p>
+      <a href="">Voltar ao catálogo</a>
+    </main>
   )
 }
