cd294135c0ecc88709d2ea7842fd0f8705fdeb85
Matheus Pazinati <matheuspazinati@gmail.com>
2023-03-08 05:53:11 -0300

    Redireciona o usuario caso a pagina de sucesso seja chamada sem um id de sessao

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -26,6 +26,16 @@ export default function Success({ customer, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+  
   const sessionId = String(query.session_id)
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ['line_items', 'line_items.data.price.product']
