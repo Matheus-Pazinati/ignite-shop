fba7cb41430eb68c56d25a44566ab510fe7e9227
Matheus Pazinati <matheuspazinati@gmail.com>
2023-03-09 06:19:33 -0300

    Finaliza estilizacao do botao do Carrinho de Compra

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,24 @@ import * as Dialog from '@radix-ui/react-dialog';
 
 import { ProductsCart } from '../components/ProductsCart';
 
-import { Header } from '../styles/components/header'
+import { CartQuantityView, CartTrigger, Header } from '../styles/components/header'
 import logoImg from '../assets/logo.svg'
 import { Handbag } from 'phosphor-react'
 
 export function DefaultHeader() {
+  const teste = true
   return (
     <Header>
       <Image src={logoImg} alt="" />
       <Dialog.Root>
-        <Dialog.Trigger asChild>
+        <CartTrigger asChild>
           <button>
-            <Handbag />
+            <Handbag size={24} color={'#8D8D99'} weight={'bold'} />
           </button>
-        </Dialog.Trigger>
+        </CartTrigger>
+        <CartQuantityView view={'false'} >
+          1
+        </CartQuantityView>
         <ProductsCart />
       </Dialog.Root>
     </Header>
